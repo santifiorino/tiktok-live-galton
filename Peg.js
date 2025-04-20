89af6984877c415d6dece7161e94c3bcce98ab33
Santiago Fiorino <fiorinosanti@gmail.com>
2025-04-20 18:55:46 -0300

    peg file

---
+++ Peg.js
@@ -0,0 +1,15 @@
+import { BALL_SIZE } from './config.js';
+
+export class Peg {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    show(ctx, galtonPosToXCoord, galtonPosToYCoord) {
+        ctx.fillStyle = "#000000";
+        ctx.beginPath();
+        ctx.arc(galtonPosToXCoord(this.x), galtonPosToYCoord(this.y), BALL_SIZE / 2, 0, Math.PI * 2);
+        ctx.fill();
+    }
+} 
